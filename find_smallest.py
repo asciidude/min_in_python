3431d3df1a41276675d2fb88af7045f42d6579d0
pxpcandy <39356292+pxpcandy@users.noreply.github.com>
2021-04-25 01:48:07 -0500

    add start-end prints

--- find_smallest.py
+++ find_smallest.py
@@ -1,5 +1,4 @@
 import time
-
 def time_func(func):
   t1 = time.time()
   func()
@@ -7,7 +6,9 @@ def time_func(func):
   print(int(t2 - t1))
 
 a_lst = [ 1, 5, 2, 4 ]
-b_lst = [ 5, 8, 4, 0 ]
+b_lst = [ 5, 8, 4, -5 ]
+
+print(" -- START OF FIND_SMALLEST -- ")
 
 @time_func
 def find_smallest():
@@ -16,16 +17,22 @@ def find_smallest():
     if i < a_l_c:
       a_l_c = i
   
-  print(a_l_c) # O > 0
+  print(a_l_c)
 
   b_l_c = b_lst[0]
   for i in b_lst:
     if i < b_l_c:
       b_l_c = i
   
-  print(b_l_c) # O > 0
+  print(b_l_c)
+
+print(" -- END OF FIND_SMALLEST -- \n\n")
+
+print(" -- START OF MIN -- ")
 
 @time_func
 def find_smallest_min():
-  print(min(a_lst)) # O > 0
-  print(min(b_lst)) # O > 0
+  print(min(a_lst))
+  print(min(b_lst))
+
+print(" -- END OF MIN -- ")
